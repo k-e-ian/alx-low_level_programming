ba1e7142568a7fa7c01f8f1397c7c5bcafea57c3
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-06-30 07:29:19 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 #include <string.h>
-
+/**
+ * main - Entry point
+ *
+ * Variable type, size and assignment
+ *
+ * calling puts function, strcpy
+ *
+ * puts function
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char string[53];
 
-	strcpy(string, "Programming is like building a multilingual puzzle\n");
+	strcpy(string, "\"Programming is like building a multilingual puzzle");
 
 	puts(string);
-	
+
 	return (0);
 }
