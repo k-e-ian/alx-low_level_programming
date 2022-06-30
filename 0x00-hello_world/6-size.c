a5bc2d90669b2c114d48bad9ae8aed1b32875778
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-06-30 13:02:45 -0700

    printf, sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,12 @@
  */
 int main(void)
 {
-    char a;
-    int b;
-    long int c;
-    long long int d;
-    float e;
+    char(a);
+    int(b);
+    long int(c);
+    long long int(d);
+    float(e);
+
     printf("Size of a char: %d byte(s)\n", sizeof(a));
     printf("Size of an int: %d byte(s)\n", sizeof(b));
     printf("Size of a long int: %d byte(s)\n", sizeof(c));
