7ff658189a10ba906c1d7169447d86b758584b0e
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-06-30 07:16:07 -0700

    print with puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char string[53];
+
+	strcpy(string, "Programming is like building a multilingual puzzle\n");
+
+	puts(string);
+	
+	return (0);
+}
