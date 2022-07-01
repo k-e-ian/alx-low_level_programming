02dbc061b965a9e095eae96b665461fd80e133eb
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-07-01 08:38:13 -0700

    display numbers

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry mode
+ *
+ * variable assignment and type
+ *
+ * forloop function
+ *
+ * putchar function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph = 'a'
+
+	for (alph = 'a'; alph <= 'z'; alph++;)
+		putchar(alph);
+			putchar('\n');
+	return (0);
+}
+	
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry mode
+ *
+ * variable assignment and type
+ *
+ * forloop function
+ *
+ * putchar function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char num = '0';
+	
+	for (num = '0'; num <= '9'; num++)
+		putchar(num);
+			putchar('\n');
+	return (0);
+}
