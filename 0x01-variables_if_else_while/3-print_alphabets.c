affb004c4aba729cbceff5c8014bc939bf767b60
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-07-01 07:37:01 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry mode
+ *
+ * forloop function
+ *
+ * putchar function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph = 'a';
+	char upp = 'A';
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+		putchar(alph);
+	for (upp = 'A'; upp<= 'Z'; upp++)
+		putchar(upp);
+			putchar('\n');
+	return (0);
+}
