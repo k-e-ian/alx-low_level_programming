d52ad4c0c98e469ba63d3c83ae4f536801d34e0c
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-07-01 19:46:22 -0700

    print comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry mode
+ *
+ * variable assignment and type
+ *
+ * while loop
+ *
+ * if function
+ *
+ * putchar function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, z;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (z = '0'; z <= '9'; z++)
+		{
+			if ((a < z) & (z <= '9'))
+			{
+				putchar(a);
+					putchar(z);
+					if ((z < '9') | (a < '8'))
+						{
+							putchar(',');
+								putchar(' ');
+						}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
