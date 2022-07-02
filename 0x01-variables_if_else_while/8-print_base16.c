35aa5ccf2e06d6ac998bb9b4c6de7834d5c04715
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-07-01 18:51:14 -0700

    hex

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
 
 /**
+ * main - Entry mode
  *
+ * Variable assignment and type
  *
+ * forloop
+ * putchar function
  *
+ * variable reassignment
  *
+ * forloop
+ * putchar function
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int hex  = '0';
 
-	for (hex = '0'; hex <= 'f'; hex++)
-		putchar("%x");
+	for (hex = '0'; hex <= '9'; hex++)
+		putchar(hex);
+	{
+	int hex = 'a';
+
+	for (hex = 'a'; hex <= 'f'; hex++)
 			putchar(hex);
 				putchar('\n');
+	}
 	return (0);
 }
