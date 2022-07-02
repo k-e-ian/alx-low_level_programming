eff98e50d8892296a1bcb2a2e202874f7b1db454
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-07-01 18:16:28 -0700

    numbersz

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,13 +8,12 @@
  * forloop function
  *
  * putchar function
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char num = '0';
-	
+
 	for (num = '0'; num <= '9'; num++)
 		putchar(num);
 			putchar('\n');
