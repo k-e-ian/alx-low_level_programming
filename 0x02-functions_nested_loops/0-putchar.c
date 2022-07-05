ac22b544cae245f71510db3acb39f44992c2a340
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-07-05 04:38:00 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_putchar function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+      	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0 (Success)
+ */
+int _putchar(char c)
+{
+	putchar(char);
+	return (0);
+}
