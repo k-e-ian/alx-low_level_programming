670967aacfe6f6f32b81994eecbb67b149bb4738
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-07-05 15:49:43 -0700

    jack bauer time 0000,2359

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
+ * jack_bauer - print time
  *
+ * for loop
  *
+ * Return: void
  */
 void jack_bauer(void)
 {
-	int time = }
+	int h, m;
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
