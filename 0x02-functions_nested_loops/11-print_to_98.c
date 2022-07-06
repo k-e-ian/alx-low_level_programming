766ca5f0e075769571d04ffaf1190eb85fa57c4c
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-07-06 05:03:37 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,34 +14,17 @@
  */
 void print_to_98(int n)
 {
-	if (n == '0')
+	while (n != 98)
 	{
-		for (n = 98; n <= 98; n++)
+		printf("%i, ", n);
+		if (n > 98)
 		{
-			_putchar(n);
-			if (n <= 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-	}
-	else if (n > 98)
-	{
-		while (n >= 98)
-		{
-			_putchar(n);
-			if (n != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
 			n--;
 		}
+		else
+		{
+			n++;
+		}
 	}
-	else
-	{
-		putchar('n');
-	}
-	putchar('\n');
+	printf("98\n");
 }
