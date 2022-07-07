4767643a3f93e1191787394a5946e2ebcfa4f8a1
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-07-07 11:47:21 -0700

    print exclude

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		if (a != '2' && a != '4')
+		_putchar(a);
+	}
+	_putchar('\n');
+}
