4daa6c780f6bb2a8b51cab25c0a133cdc46278e9
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-07-07 23:46:19 -0700

    line printer

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,14 +2,24 @@
 #include "main.h"
 
 /**
+ * print_line -line printer
  *
+ * @n: integer
  *
+ * if statement
  *
+ * for loop
+ *
+ * Return: void
  */
 void print_line(int n)
 {
 	int line;
 
-	if (n <= '0');
+	if (n > 0)
 	{
-
+		for  (line = 0; line < n; line++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
