f0057fc910ed6a4a7f3c0f3ec0b1ccb5e1fb0d52
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-07-11 15:14:04 -0700

    skip 1 char

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,26 +1,18 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * void print_rev - print sting
- *
- * @s: pointer
- *
- * Return(void)
+ * print_rev - Reverses a string.
+ * @s: The string to be reversed.
  */
 void print_rev(char *s)
 {
-	int len = 0;
-	int count;
+	int len = 0, count;
 
-	while(s[count++])
-	{
+	while (s[count++])
 		len++;
-	}
 
 	for (count = len - 1; count >= 0; count--)
-	{
-		_putchar(s[count]);
-	}
+		_putchar(s[count--]);
+
 	_putchar('\n');
 }
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints one char out of two of a string.
+ * @str: The string containing characters.
+ */
+void puts2(char *str)
+{
+	int count = 0, len = 0;
+
+	while (str[count++])
+	{
+		len++;
+	}
+
+	for (count = 0; count < len; count += 2)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
+}
