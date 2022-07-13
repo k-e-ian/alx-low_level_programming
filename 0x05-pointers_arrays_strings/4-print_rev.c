f8d2e02f039f12930d765eb43df117ec165fad81
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-07-13 05:02:37 -0700

    print rev correction

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,13 +6,19 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int index = 0;
+
+	int len = 0;
 
 	while (s[index++])
+	{
 		len++;
+	}
 
 	for (index = len - 1; index >= 0; index--)
+	{
 		_putchar(s[index]);
+	}
 
 	_putchar('\n');
 }
