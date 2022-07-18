c259b97e86c3ff3f68b0cc263baba49a0d19611d
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-07-18 07:10:58 -0700

    conforming to betty

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,12 +12,12 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int index;
-	
+
 	*dest = *src;
-	
+
 	for (index = 0; index < n; index++)
 	{
 		dest[index] = src[index];
-	}	
+	}
 	return (dest);
 }
