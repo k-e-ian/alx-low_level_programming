d6c4d0f0429bb29467b76e46776d3d31cfac55dc
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-07-18 19:11:22 -0700

    my strspn() function

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,28 +1,31 @@
 #include <stdio.h>
 
 /**
+ * _strspn - count the lenght of bytes match in s from accept
  *
+ * @s: pointer to string 1
  *
+ * @accept: pointer to sting 2
  *
- *
+ * Return: Null if no matches, otherwise length in byte of matches
  */
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int num = 0;
-	int index = 0;
-	int index2 = 0;
+	int index;
+	int index2;
 
-
-	for ( ; s[index] != '\0'; index++)
+	for (index = 0; s[index] != '\0'; index++)
 	{
-		for ( ; accept[index2] != '\0'; index2++)
+		for (index2 = 0; accept[index2] != '\0'; index2++)
 		{
-			if (s[index++] == accept[index2++])
+			if (s[index] == accept[index2])
 			{
-				continue;
+				num++;
+				break;
 			}
-			else
-			num++;
+			else if (accept[index2 + 1] == '\0')
+				return (num);
 		}
 	}
 	return (num);
