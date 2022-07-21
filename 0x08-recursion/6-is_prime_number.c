2f52a19111885c4d193dda09dc5e8eca9edb1565
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-07-20 18:18:45 -0700

    prime numbers using recursion checker

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,19 +1,41 @@
-#include <stdio.h>
+#include "holberton.h"
+
+int is_divisible(int num, int div);
+int is_prime_number(int n);
 
 /**
+ * is_divisible - Checks if a number is divisible.
+ * @num: The number to be checked.
+ * @div: The divisor.
  *
+ * Return: If the number is divisible 0, else not divisible 1
+ */
+int is_divisible(int num, int div)
+{
+	if (num % div == 0)
+		return (0);
+
+	if (div == num / 2)
+		return (1);
+
+	return (is_divisible(num, div + 1));
+}
+
+/**
+ * is_prime_number - Checks if a number is prime.
+ * @n: The number to be checked.
  *
- *
+ * Return: If the integer is not prime 0, else is prime 1.
  */
 int is_prime_number(int n)
 {
-	if (n <= 1 || n % 2 == 0 || n % 3 == 0)
-	{
+	int div = 2;
+
+	if (n <= 1)
 		return (0);
-	}
-	if (n == 2 || n == 3)
-	{
+
+	if (n >= 2 && n <= 3)
 		return (1);
-	}
-	return (1);
+
+	return (is_divisible(n, div));
 }
