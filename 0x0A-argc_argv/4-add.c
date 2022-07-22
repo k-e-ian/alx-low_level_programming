78e8dfdfa330271a65e70f31ca7153ba347514b4
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-07-22 08:20:19 -0700

    addition of digits only

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,24 +12,21 @@ int main(int argc, char **argv)
 {
 	int index = 1;
 	int sum = 0;
+	int count = 0;
 	
-	if (argc > 1)
+	for ( ; index < argc; index++)
 	{
-		for ( ; index < argc; index++)
+		for ( ; argv[index][count]; count++)
 		{
-			if (argc > '9' || argc < '0')
+			if (argv[index][count] < '0' || argv[index][count] > '9')
 			{
 				printf("Error\n");
 				return (1);
 			}
-			else
-				sum += atoi(argv[index]);
+
 		}
-		printf("%d\n", sum);
+		sum += atoi(argv[index]);
 	}
-	else 
-	{
-		printf("%d\n", sum);
-	}
+	printf("%d\n", sum);
 	return (0);
 }
