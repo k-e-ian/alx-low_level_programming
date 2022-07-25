542848309f9c6a95c71bd9545540c401056f89bb
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-07-25 07:04:37 -0700

    my pwn strcat() function with malloc

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - a function that concatenates two strings.
+ *
+ * @s1: pointer to string1
+ *
+ * @s2: pointer to string2
+ *
+ * Return: concated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int sizesum;
+	int size1;
+	int size2;
+	int index1 = 0;
+	int index2 = 0;
+	int index3 = 0;
+	char *concat;
+
+	for ( ; s1[index1] != '\0'; index1++)
+	{
+		size1++;
+	}
+	for ( ; s2[index2] != '\0'; index2++)
+	{
+		size2++;
+	}
+	sizesum = size1 + size2;
+	concat = malloc((sizeof(char) * sizesum) + 1);
+
+	while (*s1 != '\0')
+	{
+		concat[index3] = *s1;
+		s1++;
+		index3++;
+	}
+	while (*s2 != '\0')
+	{
+		concat[index3] = *s2;
+		s2++;
+		index3++;
+	}
+
+	concat[index3] = '\0';
+	return (concat);
+}
