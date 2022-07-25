495854c53249dcba4989f41bb69ed385a4bee77c
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-07-25 02:30:28 -0700

    adding condition (or NULL if it fails)

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int index = 0;
 	char *array = malloc(sizeof(char) * size);
 
-	if (size == 0)
+	if (size == 0 || array == NULL)
 	{
 		return (NULL);
 	}
