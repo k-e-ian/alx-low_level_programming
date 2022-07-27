64857d6fc4094468d7eab430c863d912ef4c5c7c
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-07-27 08:16:28 -0700

    include right size for malloc

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,13 +12,19 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int count = 0, index = 0;
+	int count = 0, index = 0, size = 0, index1 = 0, index2 = 0;
 	char *concat, *word;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	concat = malloc(sizeof(int) * ac + 1);
+	for ( ; index1 < ac; index1++)
+	{
+		for ( ; av[index1][index2]; index2++)
+			size++;
+	}
+
+	concat = malloc(sizeof(char) * size + 1);
 
 	if (concat == NULL)
 		return (NULL);
