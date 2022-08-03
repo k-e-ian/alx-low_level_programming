c0b9f3fca52903eb1f853ed3572b12f918cb0d74
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-08-03 23:56:53 +0300

    calc starting point

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "3-calc.h";
+
+/**
+ * main - Entry point
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 success
+ */
+int main(int argc, int **argv)
+{
+	return (0);
+}
