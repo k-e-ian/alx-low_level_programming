f0d9db04950721bae8a33b198a06df5c9a18b63d
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-08-05 05:51:47 +0300

    betty compliant

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -5,21 +5,19 @@
  * get_op_function - function selector
  * @s: the operator as argument
  * Return: pointer to right function
- *
  */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        	{"+", op_add},
-        	{"-", op_sub},
-        	{"*", op_mul},
-        	{"/", op_div},
-        	{"%", op_mod},
-        	{NULL, NULL}
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
 	};
-	int i;
-	i = 0;
-	
+	int i = 0;
+
 	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
 
