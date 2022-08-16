a55cc6a3080fa151ac8f3c2fb16aa7721c9f8156
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-08-16 14:00:49 +0300

    ensuring error handling && returning NULL is appropriatly done

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,11 +10,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int count;
 
-	if (!head)
-		return (NULL);
-
 	for (count = 0; count < index; count++)
 	{
+		if (!head)
+			return(NULL);
 		head = (*head).next;
 	}
 	return (head);
