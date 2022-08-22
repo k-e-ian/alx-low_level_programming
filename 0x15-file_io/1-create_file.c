eaab263a5394aa0ddbff9e952102d4bd25893119
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-08-22 19:12:15 +0300

    minor rectifications for when text is NULL

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,11 +13,15 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
+	fd = 0;
+	while (text_content[fd])
 	{
-		for (fd = 0; text_content[fd];)
+		if (text_content != NULL)
 			fd++;
 	}
+
+	if (!text_content)
+		opEn = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	opEn = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wrIte = write(opEn, text_content, fd);
