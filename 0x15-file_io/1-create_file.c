17c3953226e3e169decb1f2bbb7fbfc529e25add
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-08-22 19:27:03 +0300

    more corrections on if text is NULL

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,10 +15,7 @@ int create_file(const char *filename, char *text_content)
 
 	fd = 0;
 	while (text_content[fd])
-	{
-		if (text_content != NULL)
-			fd++;
-	}
+		fd++;
 
 	opEn = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
