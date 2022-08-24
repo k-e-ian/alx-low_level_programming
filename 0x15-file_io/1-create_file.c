0799ef0772e04aebcbc3bdb02819a22a8a4afdfd
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-08-24 10:00:28 -0700

    create a file function-corrections

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,17 +13,14 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = 0;
-	while (text_content[fd])
-		fd++;
+	if (text_content != NULL)
+	{
+		fd = 0;
+		while (text_content[fd])
+			fd++;
+	}
 
 	opEn = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (opEn == -1)
-		return (-1);
-
-	while (text_content && text_content[fd])
-		fd++;
 
 	if (text_content == NULL)
 	{
@@ -32,7 +29,7 @@ int create_file(const char *filename, char *text_content)
 	else
 		wrIte = write(opEn, text_content, fd);
 
-	if (wrIte == -1)
+	if (wrIte == -1 || opEn == -1)
 		return (-1);
 
 	close(opEn);
