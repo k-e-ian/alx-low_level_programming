b82a4f6c1baf53a0a5dd46d6f152b7fbf8913d12
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-10-04 12:40:23 +0300

    ctypes in python

--- 0x18-dynamic_libraries/operators.c
+++ 0x18-dynamic_libraries/operators.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
 
-int _add(int a, int b);
-int _sub(int a, int b);
-int _mul(int a, int b);
-int _div(int a, int b);
-int _mod(int a, int b);
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
 
-int _add(int a, int b)
+int add(int a, int b)
 {
 	return (a + b);
 }
 
-int _sub(int a, int b)
+int sub(int a, int b)
 {
 	return (a - b);
 }
 
-int _mul(int a, int b)
+int mul(int a, int b)
 {
 	return (a * b);
 }
 
-int _div(int a, int b)
+int div(int a, int b)
 {
 	if (b != 0)
 		return (a / b);
 	return (0);
 }
 
-int _mod(int a, int b)
+int mod(int a, int b)
 {
 	if (b != 0)
 		return (a % b);
