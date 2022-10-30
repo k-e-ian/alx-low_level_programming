4767e955bd0404d636f4f9ef65bdd6de40d1b7f4
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-10-30 23:38:52 +0300

    betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,43 +10,6 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	/*
-	hash_node_t *new, *tmp;
-	unsigned long int index, i;
-
-	if (value == NULL || key == NULL || *key == '\0' || ht == NULL)
-		return (0);
-
-	i = ht->size;
-	index = key_index((const unsigned char *)key, i);
-	tmp = ht->array[index];
-	for ( ; tmp; )
-	{
-		if (tmp && strcmp(tmp->key, key) == 0)
-		{
-			free(tmp->value);
-			tmp->value = strdup(value);
-			if (tmp->value == NULL)
-				return (0);
-			return (1);
-		}
-	}
-
-	new = malloc(sizeof(hash_node_t));
-	if (!new)
-	{
-		free(new);
-		return (0);
-	}
-	new->key = strdup(key);
-	if (new->key == NULL)
-		return (0);
-	new->value = strdup(value);
-	if (new->value == NULL)
-		return (0);
-	new->next = ht->array[index];
-	ht->array[index] = new;
-	*/
 	unsigned long int i = 0;
 	hash_node_t *new = NULL, *elem = NULL;
 
