8fa2c13329dbbfc4ad0360406acbdb2d683ccb12
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-10-30 23:37:09 +0300

    collision evasion

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,6 @@
 #include "hash_tables.h"
 #include <string.h>
+
 /**
  * hash_table_set - update an element in a hash table
  * @ht: a pointer to the hash table
@@ -9,6 +10,7 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	/*
 	hash_node_t *new, *tmp;
 	unsigned long int index, i;
 
@@ -29,6 +31,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			return (1);
 		}
 	}
+
 	new = malloc(sizeof(hash_node_t));
 	if (!new)
 	{
@@ -43,6 +46,31 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	new->next = ht->array[index];
 	ht->array[index] = new;
+	*/
+	unsigned long int i = 0;
+	hash_node_t *new = NULL, *elem = NULL;
+
+	if (ht == NULL || key == NULL || (strcmp(key, "") == 0))
+		return (0);
+
+	i = key_index((unsigned char *) key, ht->size);
+	elem = ht->array[i];
+
+	if (elem && strcmp(key, elem->key) == 0)
+	{
+		free(elem->value);
+		elem->value = strdup(value);
+		return (1);
+	}
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
+
+	new->key = strdup(key);
+	new->value = strdup(value);
+	new->next = ht->array[i];
+	ht->array[i] = new;
 
 	return (1);
 }
