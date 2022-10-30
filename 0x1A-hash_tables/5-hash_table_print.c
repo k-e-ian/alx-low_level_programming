2f36d6ed8db60ed72118b57721617ec60c1dd379
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-10-30 23:53:40 +0300

    print a hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,27 +7,28 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	hash_node_t *node;
-	unsigned long int i;
-	char init = 0;
+	hash_node_t *node = NULL;
+	unsigned long int i = 0;
+	int init_comma = 0;
 
 	if (ht == NULL)
 		return;
+
 	printf("{");
+
 	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[i] != NULL)
+		node = ht->array[i];
+
+		while (node != NULL)
 		{
-			if (init == 1)
-				printf(",");
-			while (node != NULL)
-			{
-				printf("'%s': '%s'", node->key, node->value);
-				node = node->next;
-				if (node != NULL)
-					printf(", ");
-			}
-			init = 1;
+			if (init_comma == 1)
+				printf(", ");
+
+			init_comma = 1;
+			printf("'%s': '%s'", (char *) node->key,
+					(char *) node->value);
+			node = node->next;
 		}
 	}
 	printf("}\n");
